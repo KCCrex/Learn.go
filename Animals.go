6373febbec5c5424f35ee411414b674d3a0cf995
Markus <kurarugw@gmail.com>
2026-05-06 03:09:33 +0200

    farm

---
+++ Animals.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var Cow int = 10
+	var Bulls int = 4
+	var Sheeps int = 13
+	var Goats int = 5
+	fmt.Println("Сколько всего животных на ферме ?", Cow+Bulls+Sheeps+Goats)
+	total := Cow + Bulls + Sheeps + Goats
+	fmt.Println("Сколько всего животных на ферме ?", total)
+}
